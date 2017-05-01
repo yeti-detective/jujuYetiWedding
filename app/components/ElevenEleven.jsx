f2729b75d1a6f70562bf84fa63e1d6b3b6dc1237
Chris B <yeti.detective@gmail.com>
2017-05-01 19:00:37 -0400

    train commit

--- app/components/ElevenEleven.jsx
+++ app/components/ElevenEleven.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
+var elevenStyle = {
+  height: "60%",
+  width: "60%",
+  background: "rgba(200, 62, 200, 0.8)",
+  borderRadius: 20,
+  margin: "auto"
+}
+
+var numStyle = {
+  fontFamily: "'Dancing Script', cursive",
+  fontSize: "7em"
+}
+
 class ElevenEleven extends React.Component {
   render(){
     return(
-      <div>
-        
+      <div style={elevenStyle}>
+        <h2 style={numStyle}>11</h2>
       </div>
     )
   }
 }
+
+export default ElevenEleven
--- index.jsx
+++ index.jsx
@@ -3,13 +3,14 @@ import {render} from 'react-dom';
 const path = require('path');
 
 import NavBar from './app/components/NavBar.jsx'
+import ElevenEleven from './app/components/ElevenEleven.jsx'
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="container">
 				<NavBar text="Save the Date"/>
-				
+				<ElevenEleven />
 			</div>
 		);
 	}
