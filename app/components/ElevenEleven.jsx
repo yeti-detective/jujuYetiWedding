a008a9e582df6398ad7db18fc5529be5324d2528
Chris B <yeti.detective@gmail.com>
2017-05-01 23:01:30 -0400

    sleepy

--- app/components/ElevenEleven.jsx
+++ app/components/ElevenEleven.jsx
@@ -1,3 +1,8 @@
+// Oauth client ID
+// 1049830250828-cnauj8cbo3764tesb0k7763gleakgg7r.apps.googleusercontent.com
+//
+// client secret
+// -tr_UdI9Qmf_YXIU_M4al00N
 import React from 'react'
 
 var elevenStyle = {
@@ -10,14 +15,15 @@ var elevenStyle = {
 
 var numStyle = {
   fontFamily: "'Dancing Script', cursive",
-  fontSize: "7em"
+  fontSize: "7em",
+  color: "#DA0042"
 }
 
 class ElevenEleven extends React.Component {
   render(){
     return(
       <div style={elevenStyle}>
-        <h2 style={numStyle}>11</h2>
+        <h2 style={numStyle}>11/11</h2>
       </div>
     )
   }
