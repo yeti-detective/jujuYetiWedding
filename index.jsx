d1f65979b254489a1f250c6ea59d07aa8c13f13d
Chris B <yeti.detective@gmail.com>
2017-05-20 20:21:43 -0400

    woop woop

--- index.jsx
+++ index.jsx
@@ -5,7 +5,7 @@ const path = require('path');
 import NavBar from './app/components/NavBar.jsx'
 import ElevenEleven from './app/components/ElevenEleven.jsx'
 import PinterestBoard from './app/containers/PinterestBoard.jsx'
-import ZolaVid from './app/components/ZolaVid.jsx'
+// import ZolaVid from './app/components/ZolaVid.jsx'
 // import AmazonWL from './app/containers/AmazonWL.jsx'
 
 class App extends React.Component {
